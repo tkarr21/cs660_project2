73ba995523b31f1396213bbab143a11d247c8b22
Tyler Karren <tkarren22@gmail.com>
2022-04-13 15:35:39 -0600

    loose end analysis

--- work_space.py
+++ work_space.py
@@ -23,20 +23,98 @@ for item in items:
     print(item)
 
 # %%
-df['dnssec_ok'] = np.where(df['flags'].str.contains('D'), True, False)
-
-# %%
-ipv6_df = df[df['server_ip'].str.contains(':')]
-
-# %%
 df.to_pickle('2021-08-27-to-31-global.pkl')
 
 
 # %%
-df = pd.read_pickle('2021-08-27-to-31-all.pkl')
+df = pd.read_pickle('2021-08-27-to-31-global.pkl')
+
 
 # %%
+len(df.index)
+# %%
+df['dnssec_ok'] = np.where(df['flags'].str.contains('D'), True, False)
 
-# group by country and qtype and count qtypes
-df_fcountry = df.groupby(['country', 'qtype'])[
-    'country'].count().unstack('qtype').fillna(0)
+# %%
+df['ip_prot'] = np.where(df['server_ip'].str.contains(':'), 'ipv6', 'ipv4')
+# %%
+# ALL QUERIES
+df.groupby('dnssec_ok').sum().plot(
+    title='DNSSEC OK: All Queries', kind='pie', autopct='%1.0f%%', subplots=True, ylabel='')
+
+# %%
+df.head(5)
+
+# %%
+# GROUP BY COUNTRY/QTYPE (switch 'country' and 'qtype')
+country_df = mic_df.groupby(['qtype', 'dnssec_ok'])[
+    'qtype'].count().unstack('dnssec_ok').fillna(0)
+# %%
+# get top requesting countries
+country_df['total'] = country_df[False] + country_df[True]
+top_countries = country_df.nlargest(20, 'total').index.tolist()
+country_df = country_df.drop(['total'], axis=1)
+top_countries_df = country_df.loc[country_df.index.isin(top_countries)]
+
+# %%
+df.loc[]
+# %%
+top_countries_df.head(21)
+# %%
+# sum remaining countries into one row
+remaining_df = country_df[country_df.index.isin(top_countries) == False]
+
+top_countries_df.loc['Combined'] = [
+    remaining_df[False].sum(), remaining_df[True].sum()]
+
+# %%
+# plot
+top_countries_df.plot(kind='bar', logy=True,
+                      title='DNSSEC OK: top requesting ASNs', ylabel='num requests', figsize=(10, 6))
+plt.legend(loc='lower right')
+plt.show()
+# %%
+dnssec_rrtypes = ['DNSKEY', 'RRSIG', 'CDNSKEY',
+                  'NSEC', 'NSEC3', 'NSEC3PARAM', 'DS', 'CDS', 'TA', 'DLV']
+
+# %%
+top_countries_df['TpF'] = top_countries_df[True] / top_countries_df[False]
+# %%
+top_countries_df.head(21)
+# %%
+dnssec_df = country_df.loc[dnssec_rrtypes]
+dnssec_df['TpF'] = dnssec_df[True] / dnssec_df[False]
+dnssec_df.head(10)
+# %%
+df.loc[df['qtype'].isin(
+    ['NSEC', 'CDNSKEY', 'NSEC3', 'NSEC3PARAM', 'CDS', 'TA', 'DLV'])]
+# %%
+# IPv4 vs. IPv6
+ip_df = df.groupby(['ip_prot', 'dnssec_ok'])[
+    'ip_prot'].count().unstack('dnssec_ok').fillna(0)
+
+ip_df.plot(kind='bar', logy=True,
+           title='DNSSEC OK: IP protocols', ylabel='num requests')
+
+# %%
+ip_df['TpF'] = ip_df[True] / ip_df[False]
+ip_df.head(5)
+# %%
+ipv6_df = df.loc[df['ip_prot'] == 'ipv6']
+# %%
+len(ipv6_df.loc[ipv6_df['location'] != 'BYU, US'].index)
+# %%
+cname_df = df.loc[df['qtype'] == 'CNAME']
+
+# %%
+cname_df.loc[cname_df['dnssec_ok'] == False].head(100)
+# %%
+in_df = df.loc[df['country'] == 'IN']
+# %%
+in_df.loc[in_df['dnssec_ok'] == False]
+# %%
+in_df.loc[in_df['qtype'] == 'CNAME']
+# %%
+mic_df = df.loc[df['location'] == 'MICROSOFT-CORP-MSN-AS-BLOCK, US']
+mic_df.head(5)
+# %%
